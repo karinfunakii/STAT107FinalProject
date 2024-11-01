43601871e60b81c66a885a50ff73770d3d5b6953
Kashish Rai <krai009@ucr.edu>
2024-10-31 21:33:11 -0700

    placed message on main r code for team memebers

--- MainCode.R
+++ MainCode.R
@@ -1,3 +1,8 @@
 ## Code here
 
 print("Hi Kashish")
+
+print("added csv to file along with reading in the markdown document")
+
+studentperformance <- read.csv("StudentPerformanceFactors.csv")
+head(studentperformance)
